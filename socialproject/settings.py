0353a0670748e7c32bf6e0b83202dd2ee077dc27
mullaned <mullaned@gmail.com>
2016-04-25 11:19:44 +0100

    initial commit

--- socialproject/settings.py
+++ socialproject/settings.py
@@ -55,7 +55,15 @@ MEDIA_URL = "/site_media/media/"
 # Don"t put anything in this directory yourself; store your static files
 # in apps" "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PACKAGE_ROOT, "site_media", "static")
+
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
+
+STATIC_ROOT = os.path.join(PACKAGE_ROOT, "site_media", "staticfiles")
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
